f1cee0182028c0c199df2d27612b459ad6f33a1a
Brian Morales <briansmor09@gmail.com>
2018-02-03 19:20:34 -0600

    Cargando objetos

--- Cgranja.js
+++ Cgranja.js
@@ -25,3 +25,28 @@ function randm(min,max){
   var res=Math.floor(Math.random()*(max-min+1))+min;
   return res;
 }
+
+//objetos
+fond.imagen=new Image();
+fond.imagen.src=fond.url;
+fond.imagen.addEventListener("load",dibfondo);
+
+cerdo.imagen=new Image();
+cerdo.imagen.src=cerdo.url;
+cerdo.imagen.addEventListener("load",dibcerdo);
+
+pollo.imagen=new Image();
+pollo.imagen.src=pollo.url;
+pollo.imagen.addEventListener("load",dibpollo);
+
+vaca.imagen=new Image();
+vaca.imagen.src=vaca.url;
+vaca.imagen.addEventListener("load",dibvaca);
+
+
+
+
+//funcion dibujar
+function dibujar(){
+
+}
