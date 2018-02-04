07ac97620211488b697af80bdb21fda8c2fce492
Brian Morales <briansmor09@gmail.com>
2018-02-03 19:08:10 -0600

    Creando objetos y funcion aleatoria

--- Cgranja.js
+++ Cgranja.js
@@ -1,1 +1,27 @@
-alert("Funciona");
+//Cargar canvas
+var gr=document.getElementById("lienzo");
+var papel=gr.getContext("2d");
+
+// Crear objetos
+var fond={
+  url:"tile.png",
+  loadOk: false
+}
+var cerdo={
+  url:"cerdo.png",
+  loadOk: false
+}
+var pollo={
+  url:"pollo.png",
+  loadOk:false
+}
+var vaca={
+  url:"vaca.png",
+  loadOk:false
+}
+
+//funcion aleatorio
+function randm(min,max){
+  var res=Math.floor(Math.random()*(max-min+1))+min;
+  return res;
+}
